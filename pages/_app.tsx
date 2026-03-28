ca147e173676ac6f247124ecd2b3f1863a83f1af
Mcivan <mcivan98@gmail.com>
2026-03-28 14:53:21 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css'; // Existing global styles
 import '@/styles/luxury-dark-theme.css'; // New luxury dark theme
 
 function MyApp({ Component, pageProps }: AppProps) {
